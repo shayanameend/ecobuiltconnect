1adde10a4e82ec3fd74e2b2c5016369520dca133
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-03-28 01:18:35 +0500

    Add navigation links and route definitions for public app pages

---
+++ src/lib/links.ts
@@ -0,0 +1,13 @@
+import { routes } from "@/lib/routes";
+
+const nav = [
+  routes.app.public.root,
+  routes.app.public.best,
+  routes.app.public.products,
+  routes.app.public.events,
+  routes.app.public.faq,
+];
+
+export const links = {
+  nav,
+};
---
+++ src/lib/routes.ts
@@ -0,0 +1,31 @@
+const api = {};
+
+const app = {
+  public: {
+    root: {
+      label: "Home",
+      url: "/",
+    },
+    best: {
+      label: "Best Selling",
+      url: "/best",
+    },
+    products: {
+      label: "Products",
+      url: "/products",
+    },
+    events: {
+      label: "Events",
+      url: "/events",
+    },
+    faq: {
+      label: "FAQ",
+      url: "/faq",
+    },
+  },
+};
+
+export const routes = {
+  api,
+  app,
+};
