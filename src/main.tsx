7950f6f5abce6433a22cca53aa8f435bc9296e4a
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-03-28 01:19:13 +0500

    Refactor Redux store setup: create a centralized store file and implement async actions for cart, wishlist, and seller management

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,13 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 
 import { App } from "@/App";
-import { Store } from "@/redux/store";
+import { store } from "@/lib/redux/store";
 
 import "@/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider store={Store}>
+    <Provider store={store}>
       <App />
     </Provider>
   </StrictMode>
