be712332326d992a7e66835e6d0e66eb85b7b90a
Shayan Ameen <shayan.ameen.developer@gmail.com>
2025-03-28 05:51:04 +0500

    Add authentication components and loader for user sign-in and sign-up

---
+++ src/components/layout/loader/loader.tsx
@@ -0,0 +1,10 @@
+import { cn } from "@/lib/utils";
+import { LoaderIcon } from "lucide-react";
+
+export function Loader() {
+  return (
+    <div className={cn("flex justify-center items-center w-full h-full")}>
+      <LoaderIcon className={cn("animate-spin size-8")} />
+    </div>
+  );
+}
